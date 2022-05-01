601c0f93386f4ffe7f0761069d7e51ccc912b968
Anastasiia Svidzerska <anastasiia_kharchenko@ukr.net>
2022-05-01 14:55:16 +0300

    createroot was used

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import { store } from './app/store';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
+import './index.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+import { store } from './app/store';
+
+import App from './App';
+
+const container:HTMLElement | null = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(
+   <StrictMode>
+      <Provider store={store}>
+         <App />
+      </Provider>
+   </StrictMode>
 );
 
 
