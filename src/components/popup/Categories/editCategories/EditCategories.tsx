7dd7b872d00c6af24e2dc12c3c4faad6de3f3d40
Anastasiia Svidzerska <anastasiia_kharchenko@ukr.net>
2022-05-03 12:47:32 +0300

    activeCategories state in EditCategories component was refactored

--- src/components/popup/Categories/editCategories/EditCategories.tsx
+++ src/components/popup/Categories/editCategories/EditCategories.tsx
@@ -20,39 +20,39 @@ const EditCategories: React.FC = (): JSX.Element => {
    const isChooseAllCategories: boolean = useSelector((state: RootStateOrAny) => state.categories.isChooseAllCategories);
    const isEditingCategory: boolean = useSelector((state: RootStateOrAny) => state.categories.isEditingCategory);
 
-   const [isActiveCategories, setIsActiveCategories] = useState<string[]>([temporaryCategories[0]?.name]);
+   const [activeCategories, setActiveCategories] = useState<string[]>([temporaryCategories[0]?.name]);
    const [isEditCategory, setIsEditCategory] = useState<string>("");
    const [isInputValue, setIsInputValue] = useState<string>("");
    const [charAmountLeft, setCharAmountLeft] = useState<number>(0);
 
    useEffect(() => {
-      if (isActiveCategories.length === temporaryCategories.length) {
+      if (activeCategories.length === temporaryCategories.length) {
          dispatch(setChooseAllCategories(true));
       }
-   }, [isActiveCategories.length]);
+   }, [activeCategories.length]);
 
    useEffect(() => {
       if (isChooseAllCategories) {
-         setIsActiveCategories([...temporaryCategories].map(item => item.name));
-      } else if (!isChooseAllCategories && isActiveCategories.length === temporaryCategories.length) {
-         setIsActiveCategories([]);
+         setActiveCategories([...temporaryCategories].map(item => item.name));
+      } else if (!isChooseAllCategories && activeCategories.length === temporaryCategories.length) {
+         setActiveCategories([]);
       }
    },[isChooseAllCategories]);
 
    const chooseCategory = (e: React.MouseEvent<HTMLButtonElement>):void => {
       if (!isEditingCategory) { 
-         const currentActiveCategories:string[] = [...isActiveCategories];
-         const targetValue:string = e.currentTarget.value;
+         const currentActiveCategories: string[] = [...activeCategories];
+         const targetValue: string = e.currentTarget.value;
 
-         const checkActiveCategory:boolean = currentActiveCategories.includes(e.currentTarget.value);
+         const checkActiveCategory: boolean = currentActiveCategories.includes(e.currentTarget.value);
 
             if (checkActiveCategory) {
-               setIsActiveCategories(() => {
-                  const categoriesWithoutDeleteElement:string[] = currentActiveCategories.filter(item => item !== targetValue);
+               setActiveCategories(() => {
+                  const categoriesWithoutDeleteElement: string[] = currentActiveCategories.filter(item => item !== targetValue);
                   return categoriesWithoutDeleteElement;
                });
             } else {
-               setIsActiveCategories(arr => [...arr, `${targetValue}`]);
+               setActiveCategories(arr => [...arr, `${targetValue}`]);
             }
          dispatch(setChooseAllCategories(false));
       }
@@ -78,25 +78,25 @@ const EditCategories: React.FC = (): JSX.Element => {
    }
 
    const deleteCategory = (e: React.MouseEvent<HTMLButtonElement>): void => {
-      const currentCategories:Category[] = [...temporaryCategories];
+      const currentCategories: Category[] = [...temporaryCategories];
       dispatch(setTemporaryCategories(currentCategories.filter((item) => item.id !== +e.currentTarget.id)));
       
       const deleteCategory = currentCategories.find((item) => item.id === +e.currentTarget.id);
-      setIsActiveCategories(isActiveCategories.filter((item) => item !== deleteCategory?.name));
+      setActiveCategories(activeCategories.filter((item) => item !== deleteCategory?.name));
    }
 
    const doneInputCategory = (e: React.MouseEvent<HTMLButtonElement>): void => {
-      const categoriesInput:Category[] = [...temporaryCategories];
+      const categoriesInput: Category[] = [...temporaryCategories];
       const secondElementCategory = categoriesInput.find((item) => item.name === isInputValue && item.id !== +e.currentTarget.id);
 
       if (!secondElementCategory) {
          const currentCategory = categoriesInput.findIndex((item) => item.id === +e.currentTarget.id);
-         const indexActiveCategory = isActiveCategories.findIndex((item) => item === categoriesInput[currentCategory].name);
+         const indexActiveCategory = activeCategories.findIndex((item) => item === categoriesInput[currentCategory].name);
       
          categoriesInput[currentCategory] = {...categoriesInput[currentCategory], name: isInputValue};
          dispatch(setTemporaryCategories(categoriesInput)); 
 
-         setIsActiveCategories((arr) => {
+         setActiveCategories((arr) => {
             const updateArr = [...arr];
             updateArr.splice(indexActiveCategory,1,isInputValue);
             return updateArr;
@@ -104,7 +104,7 @@ const EditCategories: React.FC = (): JSX.Element => {
       } else {
          dispatch(setTemporaryCategories(categoriesInput.filter((item) => item.id !== +e.currentTarget.id)));
          const deleteCategory = categoriesInput.find((item) => item.id === +e.currentTarget.id);
-         setIsActiveCategories(isActiveCategories.filter((item) => item !== deleteCategory?.name));
+         setActiveCategories(activeCategories.filter((item) => item !== deleteCategory?.name));
       }
       setIsEditCategory("");
       dispatch(setEditingCategory(false));
@@ -115,7 +115,7 @@ const EditCategories: React.FC = (): JSX.Element => {
       dispatch(setEditingCategory(false));
    }
 
-   const renderEditButtons = (category:Category):JSX.Element => {
+   const renderEditButtons = (category: Category): JSX.Element => {
       return (
          <>
             <button onClick={editCurrentCategory} id={`${category.id}`} disabled={isAddingNewCategory || isEditingCategory}>
@@ -132,7 +132,7 @@ const EditCategories: React.FC = (): JSX.Element => {
       )
    }
 
-   const renderDoneButtons = (category:Category):JSX.Element => {
+   const renderDoneButtons = (category: Category): JSX.Element => {
       return (
          <>
             <button onClick={doneInputCategory} id={`${category.id}`}>
@@ -152,11 +152,11 @@ const EditCategories: React.FC = (): JSX.Element => {
    const displayCategories: JSX.Element[] = temporaryCategories.map((category: Category) => {
       return (
          <li key={category.id}
-            className={isActiveCategories.find(item => item === category.name) ? 'edit-component active' : 'edit-component'}>
+            className={activeCategories.find(item => item === category.name) ? 'edit-component active' : 'edit-component'}>
             <div>
                <button
                   onClick={chooseCategory}
-                  className={isActiveCategories.find(item => item === category.name) ? 'categories-name__button active' : 'categories-name__button'}
+                  className={activeCategories.find(item => item === category.name) ? 'categories-name__button active' : 'categories-name__button'}
                   value={category.name}
                   disabled={isAddingNewCategory || isEditingCategory}
                   id={`${category.id}`}>
@@ -169,13 +169,13 @@ const EditCategories: React.FC = (): JSX.Element => {
                         autoFocus /> :
                      category.name}
                </button>
-               {isActiveCategories.find(item => item === category.name) &&
+               {activeCategories.find(item => item === category.name) &&
                   isEditingCategory &&
                   +isEditCategory === category.id ?
                   <p className='edit-component__char-amount'>{charAmountLeft} char. left</p> :
                   null}
             </div>
-            {isActiveCategories.find(item => item === category.name) ?
+            {activeCategories.find(item => item === category.name) ?
                (isEditCategory !== "" && +isEditCategory === category.id ?
                   renderDoneButtons(category) :
                   renderEditButtons(category)) :
