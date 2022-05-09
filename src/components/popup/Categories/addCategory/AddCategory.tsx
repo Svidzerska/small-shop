9341ce167b4f04f00c450ff9e770fffaed0a9cf5
Anastasiia Svidzerska <anastasiia_kharchenko@ukr.net>
2022-05-09 11:59:34 +0300

    Revert "Delete src/components/popup/Categories directory"
    This reverts commit b11d1baacda863a25edd0776a99ea6500ef7cac7.

---
+++ src/components/popup/Categories/addCategory/AddCategory.tsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector, RootStateOrAny} from 'react-redux';
+
+import './addCategory.scss';
+
+import {
+   setAddingNewCategory,
+   setTemporaryCategories,
+   setChooseAllCategories
+} from '../../../../features/categories/categoriesSlice';
+
+import { Category } from '../../../../interfaces/Category';
+
+import ConfirmButtons from '../buttonsComponents/confirmButtons/ConfirmButtons';
+
+const AddCategory: React.FC = (): JSX.Element => {
+   const dispatch = useDispatch();
+
+   const isAddingNewCategory: boolean = useSelector((state: RootStateOrAny) => state.categories.isAddingNewCategory);
+   const temporaryCategories: Category[] = useSelector((state: RootStateOrAny) => state.categories.categoriesTemporaryArray);
+
+   const [value, setValue] = useState<string>("New Category");
+   const [charAmountLeft, setCharAmountLeft] = useState<number>(20);
+   const [isEditInputField, setEditInputField] = useState<boolean>(false);
+
+   // set New Category after canceling of editing
+   useEffect(() => {setValue("New Category")}, [isAddingNewCategory]);
+
+   const selectRange = (e: React.FocusEvent<HTMLInputElement>): void => {
+      e.currentTarget.setSelectionRange(0,e.currentTarget.value.length,);
+   }
+
+   const editNewCategoryInputField = (e: React.FormEvent<HTMLInputElement>): void => {
+      setEditInputField(true);
+      if (e.currentTarget.value.length <= 20) {
+         setValue(e.currentTarget.value);
+         setCharAmountLeft(20-e.currentTarget.value.length);
+      }
+   }
+
+   const doneNewCategory = (): void => {
+      const currentCategories: Category[] = [...temporaryCategories];
+
+      const secondElementCategory: Category | undefined = currentCategories.find((item) => item.name === value);
+      
+      if (value !== "" && value !== "New Category" && !secondElementCategory) {
+         currentCategories.unshift({id: Math.random(), name: value});
+         dispatch(setChooseAllCategories(false));
+      }
+
+      dispatch(setTemporaryCategories(currentCategories));
+      dispatch(setAddingNewCategory(false));
+
+      setValue("New Category");
+      setEditInputField(false);
+   }
+
+   const cancelNewCategory = (): void => {
+      dispatch(setAddingNewCategory(false));
+      setValue("New Category");
+      setEditInputField(false);
+   }
+
+   const renderAddCategoryField = (): JSX.Element => {
+      return (
+         <div className='addField'>
+            <div>
+               <div className='addField__inputField'>
+                  <input value={value}
+                     onChange={editNewCategoryInputField}
+                     onFocus={selectRange}
+                     className="inputForEdit"
+                     autoFocus />
+               </div>
+               <ConfirmButtons className='addField__buttonResult' check={doneNewCategory} cancel={cancelNewCategory}/>
+            </div>
+            {isEditInputField &&
+               <p className='addField__char-amount'>
+                  {charAmountLeft} char. left
+               </p>
+            }
+         </div>
+      )
+   }
+
+   return (
+      <>
+         {isAddingNewCategory && renderAddCategoryField()}
+      </>
+   )
+}
+
+export default AddCategory;
---
+++ src/components/popup/Categories/showCategories/ShowCategories.tsx
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import {useSelector, RootStateOrAny} from 'react-redux';
+
+import './showCategories.scss';
+
+import { Category } from '../../../../interfaces/Category';
+
+const ShowCategories: React.FC = (): JSX.Element => {
+   const categories: Category[] = useSelector((state: RootStateOrAny) => state.categories.categoriesArray);
+
+   const [activeCategories, setActiveCategories] = useState<string[]>([]);
+
+   const chooseCategory = (e: React.MouseEvent<HTMLButtonElement>): void => {
+      const targetValue: string = e.currentTarget.value;
+
+      const checkActiveCategory = activeCategories.includes(e.currentTarget.value);
+      checkActiveCategory ?
+         setActiveCategories(activeCategories.filter(item => item !== targetValue)) :
+         setActiveCategories(arr => [...arr, `${targetValue}`]);
+   }
+
+   const displayCategories: JSX.Element[] = categories.map((category: Category) => {
+      return (
+         <li key={category.id}>
+            <button
+               onClick={chooseCategory}
+               className={activeCategories.find(item => item === category.name) ?
+                  'categories-name__button active' :
+                  'categories-name__button'}
+               value={category.name}>
+               {category.name}
+            </button>
+         </li>
+      )
+   })
+
+   const chooseAll = (): void => {
+      setActiveCategories([]);
+   }
+   
+   return (
+      <ul className='popup__categories-name'>
+         <li>
+            <button onClick={chooseAll}
+               className={activeCategories.length === 0 ? 'categories-name__button active' : 'categories-name__button'}>
+               All
+            </button>
+         </li>
+         {displayCategories}
+      </ul>
+   )
+}
+
+export default ShowCategories;
