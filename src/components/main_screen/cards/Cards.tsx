56d38992a84f5bf04365f46260382ecd724d3a28
Anastasiia Svidzerska <anastasiia_kharchenko@ukr.net>
2022-05-10 19:44:11 +0300

    Cards component was refactored

--- src/components/main_screen/cards/Cards.tsx
+++ src/components/main_screen/cards/Cards.tsx
@@ -11,16 +11,14 @@ import RenderAddCard from "./renderAddCard/RenderAddCard";
 const Cards: React.FC = (): JSX.Element => {
   const cards: Product[] = useSelector((state: RootStateOrAny) => state.products.cardArray);
 
-  const displayCards: JSX.Element[] = cards.map((card: Product) => <CardElement key={card.id} card={card} />);
-
   return (
     <section className="cards-element__row">
       <ul className="cards-element__column">
-        {displayCards.filter((_item, index) => index % 2 === 0)}
+        {cards.map((card: Product, index: number) => index % 2 === 0 && <CardElement key={card.id} card={card} />)}
         {cards.length % 2 === 0 && <RenderAddCard />}
       </ul>
       <ul className="cards-element__column">
-        {displayCards.filter((_item, index) => index % 2 !== 0)}
+        {cards.map((card: Product, index: number) => index % 2 !== 0 && <CardElement key={card.id} card={card} />)}
         {cards.length % 2 !== 0 && <RenderAddCard />}
       </ul>
     </section>
